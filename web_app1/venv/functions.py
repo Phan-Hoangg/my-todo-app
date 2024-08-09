f2f2bd70ce4982af0cd4c80d68c96ea2d98b1e84
Phan Hoang <phanhoang4504@gmail.com>
2024-08-09 19:51:01 +0700

    Initial commit

--- web_app1/venv/functions.py
+++ web_app1/venv/functions.py
@@ -1,16 +1,28 @@
+# import os
+#
+# FILEPATH = "todos.txt"
+#
+# def get_todos(filepath=FILEPATH):
+#     with open(filepath, 'r') as file_local:
+#             todos_local = file_local.readlines()
+#     return todos_local
+#
+# def write_todos(todos_arg, filepath=FILEPATH):
+#     with open(filepath, 'w') as file:
+#         file.writelines(todos_arg)
+#
+# if __name__ == "__main__":
+#     print("Hello")
+#     print(get_todos())
+
 import os
 
 FILEPATH = "todos.txt"
 
 def get_todos(filepath=FILEPATH):
+    if not os.path.isfile(filepath):
+        print(f"File {filepath} does not exist.")
+        return []
     with open(filepath, 'r') as file_local:
-            todos_local = file_local.readlines()
+        todos_local = file_local.readlines()
     return todos_local
-
-def write_todos(todos_arg, filepath=FILEPATH):
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
-if __name__ == "__main__":
-    print("Hello")
-    print(get_todos())

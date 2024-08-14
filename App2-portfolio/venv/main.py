0f8ece405c1c845fef822660ff74ffe781c58e83
Phan Hoang <phanhoang4504@gmail.com>
2024-08-15 02:39:45 +0700

    add description on the new columns

--- App2-portfolio/venv/main.py
+++ App2-portfolio/venv/main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image(r"D:\Udemy\Python Mega Course Learn Python in 60 Days, Build 20 Apps\App2-portfolio\images\Photo.jpg",width=300)
+    st.image(r"D:\Udemy\Python Mega Course Learn Python in 60 Days, Build 20 Apps\App2-portfolio\images\Photo.jpg")
 
 with col2:
     st.title("Phan Hoang")
@@ -13,3 +13,8 @@ with col2:
     Xin chao moi nguoi toi la Hoang
     """
     st.info(content)
+
+content2 = """
+Contact me
+"""
+st.write(content2)
